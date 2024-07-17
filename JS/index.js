f6d99cf7384dbfd6526d6356fc74ebdeb653218e
Mahin Sindhwani <mahinsindhwani@gmail.com>
2024-07-17 14:28:02 +0530

    New Files Added

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+function turnOn(){
+    var bulb=document.getElementById("bulb")
+    if(bulb.src.includes("off.jpg")){
+    bulb.src="../assets/on.jpg";
+    }
+    else{
+        bulb.src="../assets/off.jpg";
+    }
+}
+
+
+
+//includes
